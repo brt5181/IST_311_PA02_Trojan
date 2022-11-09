1a8c264ec942aa8ecb839fcb94cf674f7c0d32b9
brt5181 <97695371+brt5181@users.noreply.github.com>
2022-11-09 18:08:00 -0500

    Added to Shuffle and tests

--- src/com/company/DoublyLinkedListTest.java
+++ src/com/company/DoublyLinkedListTest.java
@@ -153,7 +153,22 @@ class DoublyLinkedListTest {
 
     @Test
     void testPartition() {
+        DoublyLinkedList<Album> dll = new DoublyLinkedList<>();
+        assertNull(dll.head);
+        assertNull(dll.tail);
+        ArrayList<String> artistNames = new ArrayList<>();
+        artistNames.add("Artist 1");
+        artistNames.add("Artist 2");
+        Album a1 = new Album(1, artistNames, "album 1", 1);
+        assertEquals(1, dll.append(a1).album.numberOfSongs);
 
+        Album a2 = new Album(2, artistNames, "album 2", 2);
+        assertEquals(2, dll.append(a2).album.numberOfSongs);
+
+        Album a3 = new Album(3, artistNames, "album 3", 3);
+        assertEquals(3, dll.append(a3).album.numberOfSongs);
+        dll = dll.partition(a2);
+        assertEquals("", dll.toString());
 
 
     }
